2f2accace6b48c7228088302a51ffaa40ae86c20
zhangyueqingyun <zhangyueqingyun@foxmail.com>
2022-06-22 16:40:36 +0800

    feat(cors): enable cors

--- src/blog/service.ts
+++ src/blog/service.ts
@@ -35,6 +35,32 @@ export class BlogService {
                         id: 'webkit',
                         name: 'Webkit'
                     }]
+                },
+                {
+                    id: 'asdfsdfsfd22',
+                    title: '几句话说清浏',
+                    description: '由苹果公司开发，负责将网页数据渲染为图像。',
+                    date: '2022-06-22 10:14:00',
+                    labels: [{
+                        id: 'webkit1',
+                        name: 'Webkit'
+                    }]
+                }
+            ]
+        },
+        {
+            id: 'technich2',
+            name: '生活',
+            blogs: [
+                {
+                    id: 'asdfsdfsfd',
+                    title: '几句话说清浏览器渲染过程',
+                    description: 'Webkit 是 Safari 浏览器的内核（渲染引擎），由苹果公司开发，负责将网页数据渲染为图像。',
+                    date: '2022-06-21 10:24:00',
+                    labels: [{
+                        id: 'webkit',
+                        name: 'Webkit'
+                    }]
                 }
             ]
         }]
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(8000);
 }
 bootstrap();
