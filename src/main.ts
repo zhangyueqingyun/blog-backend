a207a2114c273f35052987cf0f0e623ea736fad8
zhangyueqingyun <zhangyueqingyun@foxmail.com>
2022-06-28 11:52:24 +0800

    feat(global): add global prefix

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.setGlobalPrefix('api')
   await app.listen(8000);
 }
 bootstrap();
